c41864438a0e9a6a3ef0a6981c4a6df0100b39ca
M. Ade maulana <ademaulana464@gmail.com>
2024-12-13 12:39:31 +0700

    Fix Bugs 13 Desember 2024

--- app/Views/peserta/cetak_legitimasi_projek.php
+++ app/Views/peserta/cetak_legitimasi_projek.php
@@ -75,7 +75,15 @@
                     </tr>
                     <tr style="line-height: 30px;">
                         <td style="padding-left: 10px; vertical-align: middle;">Tanggal</td>
-                        <td style="padding-left: 10px; vertical-align: middle;" colspan="2">5 - 6 Desember 2024</td>
+                        <td style="padding-left: 10px; vertical-align: middle;" colspan="2">
+                            <?php
+                            if ($kelas == "XII AKL" || $kelas == "XII MP" || $kelas == "XII AN" || $kelas == "XII TKJ" || $kelas == "XII DKV" || $kelas == "XII PPLG" || $kelas == "XII PSPT") {
+                                echo "14 Desember 2024";
+                            } else {
+                                echo "11 - 12 Desember 2024";
+                            }
+                            ?>
+                        </td>
                     </tr>
                     <tr style="line-height: 30px;">
                         <td style="padding-left: 10px; vertical-align: middle;">Waktu</td>
